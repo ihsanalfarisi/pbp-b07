8805384d8344d0231cd684c9a08c076bd9133a6d
muhammad.ihsan06 <muhammad.ihsan06@ui.ac.id>
2021-10-26 14:07:53 +0700

    Homepage 2

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.register_request, name="signup"),
     path('login/', views.login_request, name="login"),
+    path('logout/', views.logout_request, name="logout"),
 ]
 
--- project_name/settings.py
+++ project_name/settings.py
@@ -56,7 +56,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'uni_form'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

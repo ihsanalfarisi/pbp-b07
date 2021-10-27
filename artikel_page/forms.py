b8780ec442bd7bf94ae0b67dce5e40125db28138
Amira Husna Nur Adilah <amira.husna01@ui.ac.id>
2021-10-28 04:04:49 +0700

    change time_published

--- artikel_page/forms.py
+++ artikel_page/forms.py
@@ -4,7 +4,7 @@ from .models import Artikel
 class ArtikelForm(forms.ModelForm):
     class Meta:
       model = Artikel
-      exclude = ('time_published','penulis')
+      exclude = ('date_published','penulis')
     
     error_messages = {
           'required' : 'Please Fill in the Blank'

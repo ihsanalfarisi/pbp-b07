7b7db4cf24b00a1637ccfabbfc02f1bf2140c300
Magnolia Fayza <magnolia.fayza@yahoo.com>
2021-10-27 10:13:24 +0700

    Migrate and register to admin

--- regulasi/forms.py
+++ regulasi/forms.py
@@ -1,9 +1,8 @@
 from .models import Regulasi
 from django import forms
 
-# membuat NoteForm model
+# membuat CountryForm model
 class CountryForm(forms.ModelForm):
-    # semua field yg dimiliki friend dijadiin form
     class Meta:
         model = Regulasi
         fields = "__all__"

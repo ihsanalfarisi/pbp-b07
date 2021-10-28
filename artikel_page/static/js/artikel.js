33881453f4fc88784c87a2b5b4b9f778e70ab441
Amira Husna Nur Adilah <amira.husna01@ui.ac.id>
2021-10-29 00:23:34 +0700

    add format to <p>

--- artikel_page/static/js/artikel.js
+++ artikel_page/static/js/artikel.js
@@ -63,48 +63,4 @@ $(document).ready(function(){
             
       }})
     },500);}
-
-    // $('.article-page').on("click",'.detail-artikel',function(){
-    //     var id = $(this).attr('id');
-    //     var pathname = window.location.href
-    //       $.ajax({
-    //       url: pathname+'details/'+id, 
-    //       dataType: 'json',
-    //       success: function(hasil){
-    //         $(".article-page").empty()
-    //         $(".article-page").append(`
-    //         <div class="container py-4 px-5">
-    //           <header class="border-bottom">
-    //             <div class="pricing-header p-3 pb-md-0 mx-auto">
-    //               <h3 class="display-6 fw-normal text-center">${hasil[0].fields.judul}</h3>
-    //               <p class="text-muted fs-7 text-center">
-    //               ${hasil[0].fields.penulis} <br>
-    //                 ${hasil[0].fields.date_published}
-    //               </p>
-    //               <p>${hasil[0].fields.deskripsi}</p>
-    //             </div>
-    //           </header>
-    //           <main id="article" class="container py-4 px-5">
-    //             <div id="image-div" class="border-bottom">
-                  
-    //               <img src="/media/${hasil[0].fields.gambar}" id="artikel-gambar" class="img-fluid rounded ratio" style="--bs-aspect-ratio: 75%;" alt="...">
-                  
-    //             </div>
-    //             <div class="border-bottom">
-    //               <p>${hasil[0].fields.isi}</p>
-    //             </div>
-    //             <div class="text-center">
-    //               <br>
-    //               <a type="button" href="./" class="btn btn-primary">
-    //                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-    //                   <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"></path>
-    //                 </svg>
-    //                 Kembali
-    //               </a>
-    //             </div>
-    //           </main>
-    //         </div>
-    //         `)
-    //       }})
-      // });
   });

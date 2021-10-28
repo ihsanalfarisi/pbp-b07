a63b412193a0cb8762a452e5cf5251f46ebe6e2a
Amira Husna Nur Adilah <amira.husna01@ui.ac.id>
2021-10-28 22:12:08 +0700

    change model constraint

--- artikel_page/models.py
+++ artikel_page/models.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 # Create your models here.
 class Artikel(models.Model):
-    judul = models.CharField(max_length=30)
+    judul = models.CharField(max_length=150)
     gambar = models.ImageField(upload_to='images/', default='static/image/default.jpg', blank=True)
     isi = models.TextField()
-    deskripsi = models.CharField(max_length=20) #ini bisa ditaro di views buat pengaturannya
+    deskripsi = models.CharField(max_length=250) #ini bisa ditaro di views buat pengaturannya
     penulis = models.CharField(max_length=30, default=AnonymousUser)
     date_published = models.DateField(auto_now_add=True)

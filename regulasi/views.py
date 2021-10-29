68ee838863616f13022544dc81d5425f2be70726
Magnolia Fayza <magnolia.fayza@yahoo.com>
2021-10-29 21:48:43 +0700

    Login required info regulasi

--- regulasi/views.py
+++ regulasi/views.py
@@ -4,22 +4,24 @@ from .forms import CountryForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
+@login_required(login_url = '/login')
+
 # Create your views here.
 def index(request):
-    regulasi_negara = [] # tambahan
-    negara = ["Thailand", "Singapore", "Malaysia"] # tambahan
+    regulasi_negara = []
+    negara = ["Thailand", "Singapore", "Malaysia"]
     regulasi = Regulasi.objects.all().values()
 
-    for i in regulasi: # tambahan
-        regulasi_negara.append(i['negara']) # tambahan
+    for i in regulasi:
+        regulasi_negara.append(i['negara'])
 
     form = CountryForm(request.POST or None) 
     response = {'form': form, 'regulasi': regulasi}
-    if (form.is_valid() and request.method == 'POST'): # tambahin () di is_valid
-        if (not form.cleaned_data.get("negara") in regulasi_negara): # tambahan
-            if (form.cleaned_data.get("negara") in negara): # tambahan
+    if (form.is_valid() and request.method == 'POST'):
+        if (not form.cleaned_data.get("negara") in regulasi_negara):
+            if (form.cleaned_data.get("negara") in negara):
                 form.save()
-        # return HttpResponseRedirect('detail')
+                return redirect(index)
     return render(request, 'regulasi_index.html', response)
 
 def malaysia(request):

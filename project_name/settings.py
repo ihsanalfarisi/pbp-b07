e379a1dad3b388ee32cb30fddf861779214e2c70
ameliarani <amelia.maharani02@ui.ac.id>
2021-10-29 22:08:13 +0700

    change debug to true

--- project_name/settings.py
+++ project_name/settings.py
@@ -119,7 +119,7 @@ if PRODUCTION:
     DATABASES['default'] = dj_database_url.config(
         conn_max_age=600, ssl_require=True
     )
-    DEBUG = False
+    DEBUG = True
     SECURE_SSL_REDIRECT = True
 
 

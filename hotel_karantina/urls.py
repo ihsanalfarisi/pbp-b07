7405023284f57f51c3a04c6f992ddc08219b8b90
ameliarani <amelia.maharani02@ui.ac.id>
2021-11-05 01:05:22 +0700

    Edit info hotel karantina page

--- hotel_karantina/urls.py
+++ hotel_karantina/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('json', json, name='json'),
 ]
--- hotel_karantina/views.py
+++ hotel_karantina/views.py
@@ -1,25 +1,13 @@
-from django.shortcuts import (get_object_or_404, render, HttpResponseRedirect)
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
+from django.shortcuts import (get_object_or_404, redirect, render, HttpResponseRedirect)
+from django.core import serializers
 from .models import HotelKarantina
 # from .forms import HotelForm
 
 # Create your views here.
-
 def index(request):
     hotels = HotelKarantina.objects.all()
-    # hotel_country = [] 
-    # valid_country = ["Singapore", "Malaysia", "Thailand", "Indonesia"] 
-
-    # for i in hotels: # tambahan
-    #     hotel_country.append(i['country']) 
-
-    # form = HotelForm(request.POST or None) 
-    # response = {'form': form, 'hotels': hotels}
-    # if (form.is_valid() and request.method == 'POST'):
-    #     if (not form.cleaned_data.get("country") in hotel_country): 
-    #         if (form.cleaned_data.get("country") in valid_country): 
-    #             form.save()
-    #     return HttpResponseRedirect('')
-
     response = {
         'hotels': hotels,
     }
@@ -29,9 +17,23 @@ def index(request):
         print(response['Country'])
     return render(request, 'hotel_index.html', response)
 
+# def index2(request):
+#     hotel_country = [] 
+#     valid_country = ["Singapore", "Malaysia", "Thailand", "Indonesia"] 
+#     hotels = HotelKarantina.objects.all()
 
-# def search_hotel(request):
+#     for i in hotels:
+#         hotel_country.append(i['country']) 
+
 #     form = HotelForm(request.POST or None) 
-#     if (form.is_valid and request.method == 'POST'):
-#         form.save()
-#     return render(request, 'hotel_form.html')
+#     response = {'form': form, 'hotels': hotels}
+#     if (form.is_valid() and request.method == 'POST'):
+#         if (not form.cleaned_data.get("country") in hotel_country): 
+#             if (form.cleaned_data.get("country") in valid_country): 
+#                 form.save()
+#                 return redirect(index)
+#     return render(request, 'hotel_index.html', response)
+
+def json(request):
+    data = serializers.serialize('json', HotelKarantina.objects.only('country', 'nama_hotel', 'foto', 'harga', 'detail'))
+    return HttpResponse(data, content_type="application/json")

082c74d3a28a20af2fcf2b868adec2144cb80b38
ameliarani <amelia.maharani02@ui.ac.id>
2021-11-05 18:14:08 +0700

    implement ajax on hotel page

--- hotel_karantina/static/info_hotel.js
+++ hotel_karantina/static/info_hotel.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
 
     HotelPage()
 
-    document.getElementById("myInput").addEventListener("keyup",e=>{
-      HotelPage(e.target.value)
-    })
+    document.querySelector("form.mx-auto").addEventListener("submit",e=>{
+        // alert(e.target.value)
+        e.preventDefault()
+        HotelPage(document.getElementById("myInput").value)
+        document.querySelector(".result h3 span strong").innerHTML = document.getElementById("myInput").value
+      })
 
     function HotelPage(search="") {
       var pathname = window.location.href
@@ -15,34 +18,48 @@ $(document).ready(function(){
           let hasil = res;
           if(search!==""){
             hasil = res.filter(item=>{
-              if(String(item).toLowerCase().includes(search.toLowerCase())){
+              if(String(item.fields.country).toLowerCase().includes(search.toLowerCase())){ 
                 return item
               }
             })
+          }else{
+            hasil = [];
           }
-          if(hasil.length>0){
-            html="";
-            hasil.forEach(item=>{
-              html = html + `
-              <div class="col-lg-6 col-md-6"> 
-                <div class="card" style="margin-bottom: 30px;">
-                  <div class="card-header">
-                    ${item.fields.country}
-                  </div>
-                  <img class="card-img-top" src="${item.fields.foto}" alt="hotel image">
-                  <div class="card-body">
-                    <h5 class="card-title"><strong>${item.fields.nama_hotel}</strong></h5>
-                    <p class="card-text"><strong>Harga: </strong>${item.fields.harga}</p>
-                  </div>
-                  <div class="card-footer" style="text-align: center;">
-                    <a href='${item.fields.detail}' class="btn btn-primary">Info Detail</a>
+          if(search!==""){
+            if(hasil.length>0){
+              html=`
+                <h3 style="text-align: center; margin-top: 20px; margin-bottom: 10px;">
+                  Hotel Karantina Terbaik di <span style="color: rgb(255, 0, 149);"><strong>${search}</strong></span>
+                </h3>
+                <div class="row">
+              `;
+              hasil.forEach(item=>{
+                html = html + `
+                <div class="col-lg-6 col-md-6"> 
+                  <div class="card" style="margin-bottom: 30px;">
+                    <div class="card-header">
+                      ${item.fields.country}
+                    </div>
+                    <img class="card-img-top" src="${item.fields.foto}" alt="hotel image">
+                    <div class="card-body">
+                      <h5 class="card-title"><strong>${item.fields.nama_hotel}</strong></h5>
+                      <p class="card-text"><strong>Harga: </strong>${item.fields.harga}</p>
+                    </div>
+                    <div class="card-footer" style="text-align: center;">
+                      <a href='${item.fields.detail}' class="btn btn-primary">Info Detail</a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            `
-            })
-            document.querySelector(".result .row").innerHTML = html
-          }            
+              `
+              })
+              html = html + "</div>"
+              document.querySelector(".result").innerHTML = html
+            }else{
+              document.querySelector(".result").innerHTML = `<h5 style="text-align: center; margin-top: 10px;">Harap ketik negara yang sesuai</h5>`
+            }
+          }else{
+            document.querySelector(".result").innerHTML = ""
+          }       
       }})
     }
   });
